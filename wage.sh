1866e776c446f36f6831e00b875397b73a685e6f
Shashi <shashihghebad3@gmail.com>
2021-02-25 00:26:00 -0800

    changed wage.sh

--- wage.sh
+++ wage.sh
@@ -9,4 +9,8 @@ fi
 #calculation of daily employee wage
 wage_per_hour=20
 no_of_hours=8
-echo "The Daily Employee Wage is $(( attendence * wage_per_hour * no_of_hpurs ))"
+echo "The Daily Employee Wage is $(( attendence * wage_per_hour * no_of_hours ))"
+
+wage_per_hr=20
+wage_per_day=$(( wage_per_hr * no_of_hrs_per_day ))
+echo "The daily wage of the employee is $wage_per_day "
