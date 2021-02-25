04ecde8672e16c58dd560186f6e320d2edf12f63
Shashi <shashihghebad3@gmail.com>
2021-02-25 00:32:38 -0800

    changed wage.sh

--- wage.sh
+++ wage.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-attendence=$(( RANDOM % 2 ))
-if(( attendence == 0 ))
-then
-    echo "Employee is absent"
-else
-    echo "Employee is present"
-fi
-#calculation of daily employee wage
-wage_per_hour=20
-no_of_hours=8
-echo "The Daily Employee Wage is $(( attendence * wage_per_hour * no_of_hours ))"
+#attendence check and wage calculation correspondingly
+attendence=$(( RANDOM % 3 ))
+case $attendence in 
+    0)
+        echo "Employee is absent"
+        no_of_hrs_per_day=0
+        ;;
+    1)
+        echo "Employee is full time present"
+        no_of_hrs_per_day=8
+        ;;
+    2)
+        echo "Employee is part time present"
+        no_of_hrs_per_day=4
+        ;;
+esac
 
 wage_per_hr=20
 wage_per_day=$(( wage_per_hr * no_of_hrs_per_day ))
-echo "The daily wage of the employee is $wage_per_day "
+echo "The daily wage of the employee is $wage_per_day 
